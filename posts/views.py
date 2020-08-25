2820fadc45acb1a649db8bb70722dccffe8d9e79
dddooo9 <dlaehdus99@gmail.com>
2020-08-25 22:45:56 +0900

    comment crud

--- posts/views.py
+++ posts/views.py
@@ -46,3 +46,18 @@ def create_comment(request, post_id):
         comment_content=request.POST.get('content')
         Comment.objects.create(content=comment_content, writer=current_user, post=post)
     return redirect('posts:show', post.pk)
+
+def delete_comment(request, comment_id):
+    comment=get_object_or_404(Comment, pk=comment_id)
+    post_id = comment.post.id
+    comment.delete()
+    return redirect("posts:show", post_id)
+
+def update_comment(request, comment_id):
+    comment=get_object_or_404(Comment, pk=comment_id)
+    if request.method == "POST":
+        post_id = comment.post.id
+        comment.content=request.POST.get('content')
+        comment.save()
+        return redirect('posts:show', post_id)
+    return render(request, 'posts/update_comment.html',{"comment":comment})
