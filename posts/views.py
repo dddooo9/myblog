7bab0fe7d289ed2f0eea53c8e32f3da56077895f
dddooo9 <dlaehdus99@gmail.com>
2020-09-03 21:53:46 +0900

    like count

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post, Comment
+from django.contrib.auth.decorators import login_required
+
 
 def new(request):
     return render(request, 'posts/new.html')
@@ -61,3 +63,28 @@ def update_comment(request, comment_id):
         comment.save()
         return redirect('posts:show', post_id)
     return render(request, 'posts/update_comment.html',{"comment":comment})
+
+@login_required
+def post_like(request, post_id):
+    post=get_object_or_404(Post, pk=post_id)
+    
+    if request.user in post.like_user_set.all():
+        post.like_user_set.remove(request.user)
+    else:
+        post.like_user_set.add(request.user)
+
+    # post_like, post_like_created = post.like_set.get_or_create(user=request.user)
+    # if not post_like_created:    
+    #     post_like.delete()
+    
+
+    if request.GET.get('redirect_to')=='show':
+        return redirect('posts:show', post_id)
+    else:       
+        return redirect('posts:main')
+
+@login_required
+def like_list(request):
+    # likes = Like.objects.filter(user=request.user)
+    likes = request.user.like_set.all()
+    return render(request, 'posts/like_list.html', {'likes':likes})
